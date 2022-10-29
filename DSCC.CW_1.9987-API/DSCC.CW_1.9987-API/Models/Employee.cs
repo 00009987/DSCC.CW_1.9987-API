55df70278cf819f7aa4f9f7d70bb858b29c80b0d
Jamoliddin Saidov <sjamoliddin21@gmail.com>
2022-10-29 21:20:48 +0500

    update: remove PassportNo field

--- DSCC.CW_1.9987-API/DSCC.CW_1.9987-API/Models/Employee.cs
+++ DSCC.CW_1.9987-API/DSCC.CW_1.9987-API/Models/Employee.cs
@@ -8,7 +8,6 @@ namespace DSCC.CW_1._9987_API.Model
     public class Employee
     {
         public int EmployeeId { get; set; }
-        public string PassportNO { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Position { get; set; }
